fc52a2f02d6796dd552bbd749b088ecbe635c32d
priyaV <mm.priyavaghasiya@gmail.com>
2023-05-31 16:49:21 +0530

    -init

---
+++ build/precache-manifest.543c9cad9ce8967e2fe7f4bd4fcbf32e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "936a1957d2d1a762fcde3bf6d74083cd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dcb2aefeb43f1399fe16",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "47076214c896de916525",
+    "url": "/static/css/main.a7436d16.chunk.css"
+  },
+  {
+    "revision": "dcb2aefeb43f1399fe16",
+    "url": "/static/js/2.cacb6e13.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/static/js/2.cacb6e13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47076214c896de916525",
+    "url": "/static/js/main.b79da474.chunk.js"
+  },
+  {
+    "revision": "d019236847ab67000d20",
+    "url": "/static/js/runtime-main.aedf75ba.js"
+  }
+]);
